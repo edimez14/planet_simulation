29160f96ff125f50c8a953b9c80268cf95fa47e0
edimez14 <edimez14@gmail.com>
2024-01-31 18:14:07 -0500

    i modified and added a way to zoom and unzoom, plus i can move in the system with the directional keys of the keyboard. i also added all the planets in our solar system.

--- planet_simulation.py
+++ planet_simulation.py
@@ -13,6 +13,8 @@ BLUE = (100, 149, 237)
 RED = (188, 39, 50)
 DARK_GREY = (80, 78, 81)
 BROWN = (131.48, 86.585, 59.766)
+DARK_BLUE = (2.3611, 28.333, 61.389)
+GOLD = (241.05, 191.84, 45.823)
 
 FONT = pygame.font.SysFont("comicsans", 20)
 
@@ -21,7 +23,7 @@ class Planet:
 
     AU = 149.6e6 * 1000
     G = 6.67428e-11
-    SCALE = 100 / AU
+    SCALE = 250 / AU
     TIMESTEP = 3600 * 24
 
     def __init__(self, x, y, radius, color, mass):
@@ -38,30 +40,19 @@ class Planet:
         self.x_vel = 0
         self.y_vel = 0
 
-    def draw(self, win):
-        x = self.x * self.SCALE + WIDTH / 2
-        y = self.y * self.SCALE + HEIGHT / 2
+    def draw(self, win, zoom_level, view_position):
+        x = (self.x * self.SCALE + WIDTH / 2 - view_position[0]) * zoom_level + view_position[0]
+        y = (self.y * self.SCALE + HEIGHT / 2 - view_position[1]) * zoom_level + view_position[1]
 
-        # if len(self.orbit) > 2:
-        #     update_points = []
-
-        #     for point in self.orbit:
-        #         x, y = point
-        #         x = x * self.SCALE + WIDTH / 2
-        #         y = y * self.SCALE + HEIGHT / 2
-        #         update_points.append((x, y))
-
-        #     pygame.draw.lines(win, self.color, False, update_points, 2)
-
-        pygame.draw.circle(win, self.color, (x, y), self.radius)
+        pygame.draw.circle(win, self.color, (int(x), int(y)), int(self.radius * zoom_level))
 
         if not self.sun:
-            planet_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter"]
+            planet_names = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
             name_text = FONT.render(
                 f"{planet_names[self.planet_index]}", 1, WHITE)
 
             distance_text = FONT.render(
-                f" {round(self.distance_to_sun/1000, 1)} km", 1, WHITE)
+                f" {round(self.distance_to_sun / 1000, 1)} km", 1, WHITE)
             win.blit(name_text, (x - name_text.get_width() /
                                  2, y - name_text.get_height() / 2 - 20))
             win.blit(distance_text, (x - distance_text.get_width() /
@@ -110,23 +101,26 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
+    zoom_level = 1.0
+    view_position = [0, 0]
+
     # planets and start
-    sun = Planet(0, 0, 20, YELLOW, 1.98892 * 10**30)
+    sun = Planet(0, 0, 40, YELLOW, 1.98892 * 10**30)
     sun.sun = True
 
-    mercury = Planet(0.387 * Planet.AU, 0, 2, DARK_GREY, 3.30 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 1, DARK_GREY, 3.30 * 10**23)
     mercury.y_vel = -47.4 * 1000
     mercury.set_planet_index(0)
 
-    venus = Planet(0.723 * Planet.AU, 0, 3.5, WHITE, 4.8685 * 10**24)
+    venus = Planet(0.723 * Planet.AU, 0, 2, WHITE, 4.8685 * 10**24)
     venus.y_vel = -35.02 * 1000
     venus.set_planet_index(1)
 
-    earth = Planet(-1 * Planet.AU, 0, 4, BLUE, 5.9742 * 10**24)
+    earth = Planet(-1 * Planet.AU, 0, 3, BLUE, 5.9742 * 10**24)
     earth.y_vel = 29.783 * 1000
     earth.set_planet_index(2)
 
-    mars = Planet(-1.524 * Planet.AU, 0, 3, RED, 6.39 * 10**23)
+    mars = Planet(-1.524 * Planet.AU, 0, 1.5, RED, 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
     mars.set_planet_index(3)
 
@@ -134,9 +128,19 @@ def main():
     jupiter.y_vel = -13.06 * 1000
     jupiter.set_planet_index(4)
 
-    # saturn, uranus, neptune
+    saturn = Planet(-9.582 * Planet.AU, 0, 5.5, GOLD, 5.683 * 10**26)
+    saturn.y_vel = -9.68 * 1000
+    saturn.set_planet_index(5)
 
-    planets = [sun, mercury, venus, earth, mars, jupiter]
+    uranus = Planet(-19.22 * Planet.AU, 0, 4.5, DARK_BLUE, 8.681 * 10**25)
+    uranus.y_vel = -6.81 * 1000
+    uranus.set_planet_index(6)
+
+    neptune = Planet(-30.05 * Planet.AU, 0, 4, BLUE, 1.024 * 10**26)
+    neptune.y_vel = -5.43 * 1000
+    neptune.set_planet_index(7)
+
+    planets = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
 
     while run:
         clock.tick(60)
@@ -145,10 +149,23 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    view_position[1] += 50
+                elif event.key == pygame.K_DOWN:
+                    view_position[1] -= 50
+                elif event.key == pygame.K_LEFT:
+                    view_position[0] += 50
+                elif event.key == pygame.K_RIGHT:
+                    view_position[0] -= 50
+                elif event.key == pygame.K_PLUS:
+                    zoom_level *= 1.1
+                elif event.key == pygame.K_MINUS:
+                    zoom_level /= 1.1
 
         for planet in planets:
             planet.update_position(planets)
-            planet.draw(WIN)
+            planet.draw(WIN, zoom_level, view_position)
 
         pygame.display.update()
 
